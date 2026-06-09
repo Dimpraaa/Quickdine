75736d1d5d032a1cb6871df8693447fbfa5357c9
DIMAS BAKTI PRATAMA <pdimas7121@gmail.com>
2026-06-09 15:01:30 +0700

    feat: switch frontend broadcast driver from reverb to pusher.com

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,15 +1,12 @@
 import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 
-const pusherClient = new Pusher(import.meta.env.VITE_REVERB_APP_KEY, {
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-    enabledTransports: ['ws', 'wss'],
+const pusherClient = new Pusher(import.meta.env.VITE_PUSHER_APP_KEY, {
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'ap1',
+    forceTLS: true
 });
 
 window.Echo = new Echo({
-    broadcaster: 'reverb',
+    broadcaster: 'pusher',
     client: pusherClient,
 });
